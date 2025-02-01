02813426a84f275d2a9fd11e972d61321a897f36
ValentinValentinov2006 <programingprofile@gmail.com>
2025-02-01 20:53:29 +0200

    first commit

---
+++ src/main/java/com/example/CookMaster/CookMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.CookMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CookMaster/CookMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CookMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
